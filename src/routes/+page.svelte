f42e55a8e34de668a6397c60375eccbd2074eb85
Name6996 <scwdfbbhh@gmail.com>
2025-02-18 11:39:05 +0500

    первй

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
     import { base } from "$app/paths";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome  to  SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
